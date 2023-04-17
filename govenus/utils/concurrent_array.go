cac43594df94f4ca7ba40443c03030fd512ffdc0
Alberto Elorza <albertoelorza2002@gmail.com>
2023-04-17 19:39:37 +0200

    concurrent array

---
+++ govenus/utils/concurrent_array.go
@@ -0,0 +1,45 @@
+package utils
+
+import "sync"
+
+type ConcurrentArray[T any] struct {
+	elements []T
+	rwMutex  sync.RWMutex
+}
+
+func NewArray[T any](initialCapacity int) *ConcurrentArray[T] {
+	return &ConcurrentArray[T]{
+		elements: make([]T, 0, initialCapacity),
+		rwMutex:  sync.RWMutex{},
+	}
+}
+
+func (arr *ConcurrentArray[T]) Add(item T) {
+	arr.rwMutex.Lock()
+	arr.elements = append(arr.elements, item)
+	arr.rwMutex.Unlock()
+}
+
+func (arr *ConcurrentArray[T]) Remove(item T) {
+	arr.rwMutex.Lock()
+	defer arr.rwMutex.Unlock()
+	for idx, element := range arr.elements {
+		if interface{}(element) == interface{}(item) {
+			arr.elements = append(arr.elements[:idx], arr.elements[idx+1:]...)
+		}
+	}
+}
+
+func (arr *ConcurrentArray[T]) Get(index int) T {
+	arr.rwMutex.RLock()
+	defer arr.rwMutex.RUnlock()
+	return arr.elements[index]
+}
+
+func (arr *ConcurrentArray[T]) ForEach(op func(int, T)) {
+	arr.rwMutex.RLock()
+	defer arr.rwMutex.RUnlock()
+	for idx, element := range arr.elements {
+		op(idx, element)
+	}
+}
