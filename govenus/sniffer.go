c3cc05a38aa2aa4c8c835a0f11d13318b62bf8dc
Alberto Elorza <albertoelorza2002@gmail.com>
2023-04-22 00:51:20 +0200

    sniffer

---
+++ govenus/sniffer.go
@@ -0,0 +1,25 @@
+package govenus
+
+import (
+	"fmt"
+
+	"github.com/albertoaer/venus/govenus/protocol"
+)
+
+type sniffer struct {
+	action func(protocol.Message)
+}
+
+func NewSniffer() MailBox {
+	return &sniffer{action: func(m protocol.Message) {
+		fmt.Println(m)
+	}}
+}
+
+func NewCustomSniffer(action func(protocol.Message)) MailBox {
+	return &sniffer{action: action}
+}
+
+func (sniffer *sniffer) Notify(msg protocol.Message) {
+	sniffer.action(msg)
+}
